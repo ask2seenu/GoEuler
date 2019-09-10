c4ca4e2aaa935b7a6cdf84fda3e5879c08cf1b37
Sreenivasulu Malkari <ask2seenu@gmail.com>
2019-09-10 13:33:48 +0530

    second

--- MultiplesOf3and5/main.go
+++ MultiplesOf3and5/main.go
@@ -4,8 +4,26 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
+/* Brute-force method. For every natural number below 1000, check if it's a multiple of 3 or 5. If it is, add it to the sum. */
+
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	sum := 0
+
+	for i := 0; i < 1000; i++ {
+		if i%3 == 0 {
+			sum += i
+			continue
+		}
+		if i%5 == 0 {
+			sum += i
+			continue
+		}
+	}
+	fmt.Println("sum is", sum)
 }
